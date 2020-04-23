2f7c040c9396fa359a076db5c3bf2d075d2b8c08
Iuri Costermani <iuricostermani@hotmail.com>
2020-04-23 00:49:56 -0300

    Change offset in relation to click

---
+++ helper.js
@@ -0,0 +1,32 @@
+function intersect_point(point1, point2, point3, point4) {
+   const ua = ((point4[0] - point3[0]) * (point1[1] - point3[1]) -
+             (point4[1] - point3[1]) * (point1[0] - point3[0])) /
+            ((point4[1] - point3[1]) * (point2[0] - point1[0]) -
+             (point4[0] - point3[0]) * (point2[1] - point1[1]));
+
+  const ub = ((point2[0] - point1[0]) * (point1[1] - point3[1]) -
+             (point2[1] - point1[1]) * (point1[0] - point3[0])) /
+            ((point4[1] - point3[1]) * (point2[0] - point1[0]) -
+             (point4[0] - point3[0]) * (point2[1] - point1[1]));
+
+  const x = point1[0] + ua * (point2[0] - point1[0]);
+  const y = point1[1] + ua * (point2[1] - point1[1]);
+
+  return [x, y]
+}
+
+function get_min(a1, b1, a2, b2, z){
+  let bw = w+2;
+  x1 = 0
+  x2 = width
+  y1 = x1*a1 + b1;
+  y2 = x2*a1 + b1;
+  y3 = -x1*a2 + b2;
+  y4 = -x2*a2 + b2;
+
+  let min = intersect_point([x1,y1], [x2,y2], [x1,y3], [x2,y4])
+  min = !z ? floor((-min[0] / bw) + (min[1] / (bw*0.75))) : floor((min[1] /(bw*0.75)) + (min[0] /bw));
+  // min = floor((-min[0] / bw) + (min[1] / (bw*0.75)));
+  console.log(min)
+  return min
+}
--- sketch.js
+++ sketch.js
@@ -1,45 +1,97 @@
 let w = 22;
 let angle = 0;
+let speed = 0.07;
 let magic_angle;
+let limit;
+let off;
+let _off;
+let disX;
+let disZ;
+
+function setParams(){
+  w = 22;
+  off = min(width, height);
+  _off = max(width, height);
+  limit = floor((off*0.9)*sin(magic_angle)/(w+2));
+  if(limit < 15){
+    w = floor((off*0.9)*sin(magic_angle)/15 - 2);
+    limit = 15
+  }
+}
 
 function setup() {
-  // magic_angle = atan(1/sqrt(2));
   magic_angle = QUARTER_PI;
 
   createCanvas(windowWidth, windowHeight, WEBGL);
   ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 2000);
+
+  setParams();
+  disX = limit/2;
+  disZ = limit/2;
+}
+
+function mouseClicked(){
+  angleMode(DEGREES);
+  let off = min(width, height);
+  let limit = floor((off*0.9)*sin(45)/(w+2));
+
+  off = floor(width/2 - off/2*0.9 + w*0.5 -2)
+  let l = 1.75*limit*(w+2)*sin(35.4);
+
+  if(   mouseX*tan(35.4) + height/2 - off*tan(35.4) - w >= mouseY
+        && -mouseX*tan(35.4) + height/2 + off*tan(35.4) -w <= mouseY
+        && -mouseX*tan(35.4) + height/2 + off*tan(35.4) + l >= mouseY
+        && mouseX*tan(35.4) + height/2 - off*tan(35.4) - l <= mouseY ){
+    let bw = w+2;
+
+    sx = mouseX ;
+    sz = mouseY;
+
+    x = floor((-sx / bw) + (sz / (bw*0.75)));
+    z = floor((sz /(bw*0.75)) + (sx /bw));
+
+    minX = get_min(tan(35.4), height/2 - off*tan(35.4) -w, tan(35.4), height/2 + off*tan(35.4), 0)
+    maxX = get_min(tan(35.4), height/2 - off*tan(35.4) - l, tan(35.4), height/2 + off*tan(35.4), 0)
+    minZ = get_min(tan(35.4), height/2 - off*tan(35.4) - w, tan(35.4), height/2 + off*tan(35.4), 1)
+    maxZ = get_min(tan(35.4), height/2 - off*tan(35.4) -w, tan(35.4), height/2 + off*tan(35.4) + l, 1)
+
+    disX = floor(map(x, minX, maxX, 0, limit));
+    disZ = floor(map(z, minZ, maxZ, 0, limit));
+  }
+  angleMode(RADIANS);
 }
 
 function draw() {
   background(10);
+  stroke(0)
 
   ambientMaterial(255);
-  // directionalLight(80, 0, 195, -1, 1, 0);
   directionalLight(0, 153, 195, -1, 1, 0);
   directionalLight(62, 227, 183, 0, -1, -1);
 
-  let off = min(width, height);
-  translate((-off/2 + w + 100), 0, 0);
+  translate(floor(-off/2*0.9 + w), 0, 0);
   rotateX(QUARTER_PI);
-  rotateY(magic_angle);
-
-  let limit = floor((off -200)*sin(magic_angle)/(w+2));
+  rotateY(QUARTER_PI);
 
   for(let z = 0; z < limit; z++){
     for(let x = 0; x < limit; x++){
-      let offset = dist(x,z, limit/2, limit/2);
-      let scl = map(offset, 0, limit/2, 0.1, 1)
-      let h = map(sin(angle+offset*scl/2), -1, 1, 60, limit*20)
+      let offset = dist(x,z, disX, disZ);
+      let scl = map(offset, 0, limit*sqrt(2)/2, 0.1, 1);
+      let h = map(sin(angle+offset*scl*0.75), -1, 1, 60, limit*20);
       push();
       translate((w + 2)*x, 0, (w + 2)*z);
       box(w,h,w);
       pop();
     }
   }
-  angle-=0.07;
+  angle -= speed;
+
+  x1 = 0;
+
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight, WEBGL);
   ortho(-width / 2, width / 2, height / 2, -height / 2, 0, 2000);
+  setParams();
 }
